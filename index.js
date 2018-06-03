5bdba7ea05a7863e144a0fb90a7ed0f427ccc63a
Emisael Calderon <emisael.calderon@indiciumsolutions.com.mx>
2018-06-02 23:11:24 -0500

    Se configuro node.js 4

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var http = require('http')
+var fs = require("fs")
 
 http.createServer(function(request,response){
-	response.writeHead(200,{"Content-Type":"text/plain"})
-	response.end("Hola Mundo Segundo intento")
+	//response.writeHead(200,{"Content-Type":"text/plain"})
+	//response.end("Hola Mundo Segundo intento")
+	fs.readFile("chapter1/index.html", function(err, data){
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.write(data);
+	response.end();
+}
 }).listen(process.env.PORT)
