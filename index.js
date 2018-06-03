debdf979fdd1ef62d741b120f8cd34d1e22d500a
Emisael Calderon <emisael.calderon@indiciumsolutions.com.mx>
2018-06-02 23:52:34 -0500

    Se agrego node-static

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var fileServer = new nStatic.Server('./chapter1/js');
 http.createServer(function(request,response){
 	//response.writeHead(200,{"Content-Type":"text/plain"})
 	//response.end("Hola Mundo Segundo intento")
-	//fileServer.serve(request, response);
+	fileServer.serve(request, response);
 	
 	fs.readFile("chapter1/index.html", function(err, data){
 		response.writeHead(200, {'Content-Type': 'text/html'});
