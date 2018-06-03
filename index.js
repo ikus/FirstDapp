7c5e48789c516df7cf3b7f77b0e38d0be2b2c50b
Emisael Calderon <emisael.calderon@indiciumsolutions.com.mx>
2018-06-03 00:06:06 -0500

    Se corrigio referencia.

--- index.js
+++ index.js
@@ -5,17 +5,25 @@ var app = express();
 var nStatic = require('node-static');
 var fileServer = new nStatic.Server('./chapter1/js');
 
+
+var finalhandler = require('finalhandler')
+var serveStatic = require('serve-static')
+var serve = serveStatic('chapter1/js', {'index': ['index.js']})
+
+
 http.createServer(function(request,response){
 	//response.writeHead(200,{"Content-Type":"text/plain"})
 	//response.end("Hola Mundo Segundo intento")
-	/*
+	//fileServer.serve(request, response)
+	serve(request, response, finalhandler(request, response))
+
 	fs.readFile("chapter1/index.html", function(err, data){
 		response.writeHead(200, {'Content-Type': 'text/html'});
 		response.write(data);
 		response.end();
-	})*/
+	})
 	
-	fileServer.serve(request, response);
+	
 }).listen(process.env.PORT)
 
 
