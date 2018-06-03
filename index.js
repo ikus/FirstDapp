01f02eba4401f4351d9de89dd166ca5c24277980
Emisael Calderon <emisael.calderon@indiciumsolutions.com.mx>
2018-06-02 23:15:43 -0500

    Se agrego Procefile.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require("fs")
 http.createServer(function(request,response){
 	//response.writeHead(200,{"Content-Type":"text/plain"})
 	//response.end("Hola Mundo Segundo intento")
-	fs.readFile("chapter1/index.html", function(err, data){
+	fs.readFile("index.js", function(err, data){
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	response.write(data);
 	response.end();
