152399a863a1cad32de6b2b7b6847fa3f54ae786
Emisael Calderon <emisael.calderon@indiciumsolutions.com.mx>
2018-06-02 23:37:47 -0500

    Se configuro node.js 3

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var http = require('http')
 var fs = require("fs")
 var express = require('express');
 var app = express();
+var nStatic = require('node-static');
+var fileServer = new nStatic.Server('./chapter1');
 
 http.createServer(function(request,response){
 	//response.writeHead(200,{"Content-Type":"text/plain"})
 	//response.end("Hola Mundo Segundo intento")
+	fileServer.serve(req, res);
+	
 	fs.readFile("chapter1/index.html", function(err, data){
 		response.writeHead(200, {'Content-Type': 'text/html'});
 		response.write(data);
@@ -15,4 +19,4 @@ http.createServer(function(request,response){
 
 
 //setting middleware
-app.use(express.static('/chapter1')); //Serves resources from public folder
+//app.use(express.static('/chapter1')); //Serves resources from public folder
